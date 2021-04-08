1ac5f4a4fc683601fa4b39584b1002120423e7fe
apple <apple@apple-MacBook-Air.local>
2021-04-08 18:46:00 +0200

    object create

--- gyakorlas2.py
+++ gyakorlas2.py
@@ -1,14 +1,14 @@
 class User():
-    def __init__(self, email, nev, signincount, premium, password):
+    def __init__(self, email, name, premium, password):
         self.email = email
-        self.nev = nev
-        self.signincount = signincount
+        self.name = name
         self.premium = premium
         self.password = password
+        self.signincount = 0
 
     def __str__(self):
-        return f"The user's email is {self.email} the name is {self.nev} the number of sign in is {self.signincount} and is a {self.premium} user."
-
+  #      return f"The user's email is {self.email} the name is {self.name} the number of sign in is {self.signincount} and is a {self.premium} user."
+        return '1'
 
     def setEmail(self, email):
         self.email = email
@@ -16,7 +16,35 @@ class User():
     def getEmail(self):
         return self.email
 
+    def setName(self, name):
+        self.name = name
 
+    def getName(self):
+        return self.name
+
+#nem szetteljuk, mert belulrol allitja magat
+#   def setSignInCount(self, signincount):
+#        self.signincount = signincount
+
+    def getSignInCount(self):
+        return self.signincount
+
+    def setPremium(self, premium):
+        self.premium = premium
+
+    def getPremium(self):
+        return self.premium
+
+    def setPassword(self, password):
+        self.password = password
+
+#nem akarjuk hogy barki lassa
+#    def getPassword(self):
+#        return self.password
+
+
+Tamas = User("example@examle.com", "Lajos", True, '1234')
+print(str(Tamas))
 
 
 
@@ -41,7 +69,7 @@ class User():
 
 #                 break
 #             else:
-#                 print("menja rákba")
+#                 print("menja rakba")
 #                 # tell them it is wrong and have them retry, stay in loop
 #     return user(email, nev, signincount, premium, password)
 
